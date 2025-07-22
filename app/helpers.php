fcc06003bfb043514989296d2b453229227e21cf
wangzhen <wdsj002@126.com>
2025-07-22 18:59:17 +0800

    新增自定义辅助函数文件

---
+++ app/helpers.php
@@ -0,0 +1,8 @@
+/*
+ * @Author: wangzhen wdsj002@126.com
+ * @Date: 2025-07-22 18:57:33
+ * @LastEditors: wangzhen wdsj002@126.com
+ * @LastEditTime: 2025-07-22 18:58:29
+ * @FilePath: \larabbs\app\helpers.php
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
