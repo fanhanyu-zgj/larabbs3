02bc4b409631b4ef66892dba42a8b42a66004e51
wangzhen <wdsj002@126.com>
2025-07-31 14:54:38 +0800

    上传头像

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,7 +3,7 @@
  * @Author: wangzhen wdsj002@126.com
  * @Date: 2025-07-22 17:33:48
  * @LastEditors: wangzhen wdsj002@126.com
- * @LastEditTime: 2025-07-31 11:38:55
+ * @LastEditTime: 2025-07-31 11:40:33
  * @FilePath: \larabbs\app\Models\User.php
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -29,7 +29,8 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
-        'introduction'
+        'introduction',
+        'avatar'
     ];
 
     /**
--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,5 +1,12 @@
 <?php
-
+/*
+ * @Author: wangzhen wdsj002@126.com
+ * @Date: 2025-07-22 17:33:48
+ * @LastEditors: wangzhen wdsj002@126.com
+ * @LastEditTime: 2025-07-31 14:53:22
+ * @FilePath: \larabbs\bootstrap\app.php
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
 /*
 |--------------------------------------------------------------------------
 | Create The Application
